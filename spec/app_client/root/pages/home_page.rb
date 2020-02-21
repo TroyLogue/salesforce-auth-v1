68dc3dad3748988023128de9719da4c91d80e428
mshuheb <60902289+mshuheb@users.noreply.github.com>
2020-02-21 16:24:11 +0000

    UU3-39667 Port home page css from Test Cafe (#5)
    * Created home page object model
    
    * Added formatting and comment in re to duplicate selector
    
    * Added missing assignment operator

---
+++ spec/app_client/root/pages/home_page.rb
@@ -0,0 +1,19 @@
+require_relative '../../../shared_components/base_page'
+
+class HomePage < BasePage
+
+    NEW_REFERRAL_BTN = { css: '#incoming-new-referrals-tab' }
+    NEW_AR_BTN = { css: '#incoming-assistance-requests-tab' }
+    INCOMING_PENDING_BTN = { css: '#incoming-pending-consent-tab' }
+    ASSIGNED_IN_REVIEW = { css: '#assigned-in-referrals-tab' }
+    ASSIGNED_RECALL_BTN = { css: '#assigned-recalled-referrals-tab' }
+    REFERRAL_PENDING_BTN = { css: '#referred-out-pending-consent-tab' }
+    REFERRAL_NEEDS_ACTION_BTN = { css: '#referred-out-needs-action-tab' }
+    REFERRAL_REJECTED_BTN = { css: '#referred-out-rejected-referrals-tab' }
+    OPEN_CASES_BTN = { css: '#assigned-open-cases-tab' }
+    CLOSED_CASE_BTN = { css: '#assigned-closed-cases-tab' }
+    TIMELINE = { css: '.activity-stream' }
+    INTERCOM_IFRAME = { css: '.intercom-launcher-frame' }
+    # Revisit INTERCOM_BTN selector once selector migration plan is complete
+    INTERCOM_BTN = { css: '.intercom-launcher' }
+    CLOSE_INTERCOM_BTN = { INTERCOM_BTN }
