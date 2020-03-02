eb511349940838325fd4362d287ff9fa2ef18ab1
Tyler Sargent <tsargent@gmail.com>
2020-03-02 15:41:37 -0500

    UU3-36794: add exports nav

--- spec/app_client/root/pages/left_nav.rb
+++ spec/app_client/root/pages/left_nav.rb
@@ -3,8 +3,19 @@ require_relative '../../../shared_components/base_page'
 class LeftNav < BasePage
 
   MY_NETWORK_LINK = { css: '#nav-network' }
+  REPORTS_LINK = { css: '#nav-report' }
+  EXPORTS_LINK = { css: '.network-menu-exports-btn' }
 
   def go_to_my_network
     click(MY_NETWORK_LINK)
   end 
+
+  def go_to_reports
+    click(REPORTS_LINK)
+  end
+
+  def go_to_exports
+    go_to_reports
+    click(EXPORTS_LINK)
+  end
 end
