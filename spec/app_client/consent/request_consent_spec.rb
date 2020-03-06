d7e1f7bbdbd51fc2d862693d9f4391aefa44a78c
Leah D. Garber <leah.garber88@gmail.com>
2020-03-06 15:35:59 -0500

    base of test page - login

---
+++ spec/app_client/consent/request_consent_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../../spec_helper'
+require_relative '../auth/helpers/login'
+require_relative '../auth/pages/login_email'
+require_relative '../auth/pages/login_password'
+# require_relative './pages/assistance_requests' Not written yet
+
+describe '[Consent - Request Consent]', :consent, :app_client do 
+  include Login
+
+  let(:login_email) { LoginEmail.new(@driver) }
+  let(:login_password) { LoginPassword.new(@driver) }
+  let(:base_page) { BasePage.new(@driver) }
+
+  context('[as cc user]') do 
+    before {
+      log_in_as(Login::CC_HARVARD)
+    } 
+
+    it 'uploads consent form' do 
+    end
+
+    # need this? mailtrap... 
+    it 'requests and grants consent by email' do 
+    end
+
+
+  end
+end
