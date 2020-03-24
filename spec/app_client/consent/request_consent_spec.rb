40cf7c51e5744c8f64ccfda7870fe4ffd09377e4
Leah D. Garber <leah.garber88@gmail.com>
2020-03-23 17:57:57 -0700

    request consent via email

--- spec/app_client/consent/request_consent_spec.rb
+++ spec/app_client/consent/request_consent_spec.rb
@@ -28,27 +28,6 @@ describe '[Consent - Request Consent]', :consent, :app_client do
       expect(pending_consent_page.page_displayed?).to be_truthy
     } 
 
-    it 'adds consent to an incoming Pending Consent referral' do 
-      @first_referral_text = pending_consent_page.text_of_first_referral;
-      @second_referral_text = pending_consent_page.text_of_second_referral; 
-      pending_consent_page.open_first_consent_modal
-      expect(pending_consent_page.consent_modal_displayed?).to be_truthy
-
-      consent_modal.add_on_screen_consent 
-
-      expect(pending_consent_page.consent_modal_not_displayed?).to be_truthy
-
-      # confirm the referral we added consent to 
-      # is removed from the pending consent page on refresh
-      base_page.refresh
-      expect(pending_consent_page.page_displayed?).to be_truthy 
-      pp "refreshed page and page is displayed again"
-
-      @new_first_referral_text = pending_consent_page.text_of_first_referral
-      expect(@new_first_referral_text).to eq(@second_referral_text)
-    end
-
-=begin
     it 'requests consent by email' do 
       pending_consent_page.open_first_consent_modal
       expect(pending_consent_page.consent_modal_displayed?).to be_truthy
@@ -60,15 +39,5 @@ describe '[Consent - Request Consent]', :consent, :app_client do
       expect(notification_text).to include(Notifications::CONSENT_REQUEST_SENT)
     end
 
-    it 'requests consent by text' do 
-      pending_consent_page.open_first_consent_modal
-      expect(pending_consent_page.consent_modal_displayed?).to be_truthy
-
-      consent_modal.request_consent_by_text(ConsentModal::VALID_PHONE_NUMBER)
-
-      notification_text = notifications.success_text
-      expect(notification_text).to include(Notifications::CONSENT_REQUEST_SENT)
-    end
-=end
   end
 end
