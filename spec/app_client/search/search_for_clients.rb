cf1242d265ce4b161e49243a8f9f1b9a12434cc7
Valerie Maldonado <valeriemaldonado@Valeries-MBP.home>
2020-04-06 16:54:19 -0400

    finalized test cases

---
+++ spec/app_client/search/search_for_clients.rb
@@ -0,0 +1,36 @@
+require_relative '../../spec_helper'
+require_relative '../auth/helpers/login'
+require_relative '../auth/pages/login_email'
+require_relative '../auth/pages/login_password'
+require_relative '../root/pages/right_nav'
+require_relative '../facesheet/pages/facesheet_page'
+require_relative './pages/search_results_page'
+
+describe '[Search]', :app_client, :search do
+  include Login
+
+  let(:login_email) {LoginEmail.new(@driver) }
+  let(:login_password) {LoginPassword.new(@driver) }
+  let(:base_page) { BasePage.new(@driver) }
+  let(:search_bar) { RightNav::SearchBar.new(@driver) }
+  let(:search_results_page) { SearchResultsPage.new(@driver) }
+
+  context('[as org user]') do
+    before {
+      log_in_as(Login::ORG_YALE)
+    } 
+
+    it 'Search Bar Results Table', :uuqa_170 do
+      search_bar.search_for('E')
+      expect(search_bar.get_search_name_list.count).to be < 13
+      expect(search_bar.get_search_name_list).to all(start_with('E'))
+    end
+
+    it 'Search Page Results Table', :uuqa_663 do
+      search_bar.go_to_search_table('E')
+      expect(search_bar.are_results_not_displayed?).to be_truthy
+      expect(search_results_page.get_search_name_list).to all(start_with('E'))
+    end
+  end
+  
+end
