82648b061c7fc2b1c356314e7aa2107aa15b4bae
Leah D. Garber <leah.garber88@gmail.com>
2020-04-10 13:06:25 -0700

    updating validations

--- spec/app_client/auth/login_logout_spec.rb
+++ spec/app_client/auth/login_logout_spec.rb
@@ -24,11 +24,13 @@ describe '[Auth - Login/Logout]', :app_client, :auth, :login do
   it 'Prevents login with incorrect password', :uuqa_9 do 
     log_in_as(Login::CC_HARVARD, Login::WRONG_PASSWORD)
     expect(login_password.page_displayed?).to be_truthy
+    expect(login_password.invalid_alert_displayed?).to be_truthy
   end
 
   it 'Prevents login as unlicensed user', :uuqa_778 do 
     log_in_as(Login::NOLICENSE_MARTIN)
     expect(login_password.page_displayed?).to be_truthy
+    expect(login_password.invalid_alert_displayed?).to be_truthy
   end
 
 end
