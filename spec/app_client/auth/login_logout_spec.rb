79ae80cfe268eb58a45dca42eeb8b0b829fd1eb5
Leah D. Garber <leah.garber88@gmail.com>
2020-04-10 11:47:56 -0700

    login tests

--- spec/app_client/auth/login_logout_spec.rb
+++ spec/app_client/auth/login_logout_spec.rb
@@ -2,35 +2,33 @@ require_relative '../../spec_helper'
 require_relative '../auth/helpers/login'
 require_relative '../auth/pages/login_email'
 require_relative '../auth/pages/login_password'
-require_relative '../root/pages/left_nav'
-require_relative './pages/network_browse_map'
+require_relative '../root/pages/home_page'
+require_relative '../root/pages/right_nav'
 
-describe '[Auth - Login and Logout]', :app_client, :auth, :login do
+describe '[Auth - Login/Logout]', :app_client, :auth, :login do
   include Login
 
-  let(:left_nav) { LeftNav.new(@driver) }
+  let(:base_page) { BasePage.new(@driver) }
+  let(:home_page) { HomePage.new(@driver) }
   let(:login_email) {LoginEmail.new(@driver) }
   let(:login_password) {LoginPassword.new(@driver) }
-  let(:network_browse_map) { NetworkBrowseMap.new(@driver) }
-  let(:base_page) { BasePage.new(@driver) }
+  let(:user_menu) {RightNav::UserMenu.new(@driver) }
 
-  it 'Logs in with a valid email, then logs out' do 
-    log_in_as(CC_HARVARD)
-    # expect home page is displayed
-    # log out
+  it 'Logs in with a valid email, then logs out', :uuqa_5 do 
+    log_in_as(Login::CC_HARVARD)
+    expect(home_page.page_displayed?).to be_truthy
+    user_menu.log_out 
+    expect(login_email.page_displayed?).to be_truthy
   end
 
-  it 'Prevents login with invalid email' do 
-    #can't use log_in_as here
+  it 'Prevents login with incorrect password', :uuqa_9 do 
+    log_in_as(Login::CC_HARVARD, Login::WRONG_PASSWORD)
+    expect(login_password.page_displayed?).to be_truthy
   end
 
-  it 'Prevents login with incorrect password' do 
-    log_in_as(CC_HARVARD, WRONG_PASSWORD)
-    # expect error message 
-  end
-
-  it 'Prevents login as unlicensed user' do 
-    log_in_as(NOLICENSE_MARTIN)
+  it 'Prevents login as unlicensed user', :uuqa_778 do 
+    log_in_as(Login::NOLICENSE_MARTIN)
+    expect(login_password.page_displayed?).to be_truthy
   end
 
 end
