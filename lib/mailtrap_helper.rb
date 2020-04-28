cd10a978af0aa3c8f2aae96fe6ec940783152fe2
Leah D. Garber <leah.garber88@gmail.com>
2020-04-28 09:15:19 -0700

    updating env variables for auth_url and mailtrap_id

--- lib/mailtrap_helper.rb
+++ lib/mailtrap_helper.rb
@@ -3,15 +3,15 @@ require 'json'
  
 module MailtrapHelper
 
-  STAGING_ID = '99406' 
   API_TOKEN = '6b245be8870dcd04019455d0a918421b'
   PASSWORD_RESET_SUBJECT = "Reset password instructions"
 
   def find_reset_link(str) 
-    str.match(/https:\/\/app.auth.uniteusdev.com\/secret\/edit\?client_id=\w{10}&amp;reset_password_token=\S{20}/)[0]
+    auth_url = ENV['auth_url'].gsub('/', '\/')    
+    str.match(/#{auth_url}\/secret\/edit\?client_id=\w{10}&amp;reset_password_token=\S{20}/)[0]
   end
 
-  def get_messages(mailbox_id: STAGING_ID) 
+  def get_messages(mailbox_id: ENV['mailtrap_id']) 
     response = HTTP.get("https://mailtrap.io/api/v1/inboxes/#{mailbox_id}/messages", 
                         :params => {:api_token => API_TOKEN})
     if response.status.success? 
@@ -26,7 +26,7 @@ module MailtrapHelper
     get_html_of_message(message_id: message_id)
   end
 
-  def get_html_of_message(message_id:, mailbox_id: STAGING_ID)
+  def get_html_of_message(message_id:, mailbox_id: ENV['mailtrap_id'])
     response = HTTP.get("https://mailtrap.io/api/v1/inboxes/#{mailbox_id}/messages/#{message_id}/body.html", 
             :params => {:api_token => API_TOKEN})
     if response.status.success?
