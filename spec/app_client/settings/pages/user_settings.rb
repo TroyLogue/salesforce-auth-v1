3aa5b500bcc5933964b338259674ad1d914b6955
Leah D. Garber <leah.garber88@gmail.com>
2020-04-28 07:28:32 -0700

    join is_displayed? calls

--- spec/app_client/settings/pages/user_settings.rb
+++ spec/app_client/settings/pages/user_settings.rb
@@ -7,13 +7,13 @@ class UserSettings < BasePage
   SUBMIT_BUTTON = { css: "#user-settings-tabs > div.ui-base-card.ui-base-card--bordered > div.ui-gradient > div > div:nth-child(4) > div > div > div:nth-child(1) > form > div.row > div > div > button" } # to do - add id to this button 
 
   def page_displayed? 
-    is_displayed?(SETTINGS_DIV)
+    is_displayed?(SETTINGS_DIV) &&
 
     # page lands on 'Account Information' tab
     # so these should be displayed
-    is_displayed?(NEW_PASSWORD_INPUT) 
-    is_displayed?(CONFIRM_NEW_PASSWORD)
-    is_displayed?(SUBMIT_BUTTON) 
+    is_displayed?(NEW_PASSWORD_INPUT) &&
+    is_displayed?(CONFIRM_NEW_PASSWORD) &&
+    is_displayed?(SUBMIT_BUTTON)
   end 
 
   def change_password(new_pw)
