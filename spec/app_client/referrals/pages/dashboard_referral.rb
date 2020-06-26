7ae71f28190e696aeb3335d15de52f789b0434e4
Valerie Maldonado <valeriemaldonado@Valeries-MBP.home>
2020-06-26 14:50:06 -0400

    in review referral

--- spec/app_client/referrals/pages/dashboard_referral.rb
+++ spec/app_client/referrals/pages/dashboard_referral.rb
@@ -1,6 +1,17 @@
 require_relative '../../../../lib/file_helper'
 
 class Referral < BasePage
+  REFERRAL_STATUS = { css: '.detail-status-text.uppercase' }
+
+  TAKE_ACTION_DROP_DOWN = { css: '.action-select-container' }
+  TAKE_ACTION_HOLD_OPTION =  { css: 'div[data-value="holdForReview"]' }
+
+  HOLD_REFERRAL_MODAL = { css: '.dialog.open.large .hold-modal-form' }
+  HOLD_REFERRAL_REASON_DROPDOWN = { css: '.referral-reason-field' }
+  HOLD_REFERRAL_REASON_OPTION = { css: '.is-active .choices__item.choices__item--choice.choices__item--selectable' }
+  HOLD_REFERRAL_NOTE = { css: '.hold-modal-form #noteInput' }
+  HOLD_REFERRAL_BTN = { css: '#hold-referral-hold-btn' }
+
   DOCUMENT_ADD_LINK = { css: '#upload-document-link' }
   DOCUMENT_ATTACH_MODAL = { css: '.dialog.open.large'}
   DOCUMENT_ATTACH_BTN = { css: '#upload-submit-btn' }
@@ -14,9 +25,31 @@ class Referral < BasePage
   DOCUMENT_REMOVE_MODAL = { css: '.dialog.open.mini'}
   DOCUMENT_REMOVE_BTN = { css: '.confirmation-dialog__actions--confirm'}
 
+  IN_REVIEW_STATUS = 'IN REVIEW'
 
   def go_to_new_referral_with_id(referral_id:)
     get("/dashboard/new/referrals/#{referral_id}")
+    wait_for_spinner
+  end
+
+  def go_to_in_review_referral_with_id(referral_id:)
+    get("/dashboard/referrals/in-review/#{referral_id}")
+    wait_for_spinner
+  end
+
+  def status
+    text(REFERRAL_STATUS)
+  end
+
+  def hold_for_review_action(note:)
+    click(TAKE_ACTION_DROP_DOWN)
+    click(TAKE_ACTION_HOLD_OPTION)
+    is_displayed?(HOLD_REFERRAL_MODAL)
+    click(HOLD_REFERRAL_REASON_DROPDOWN)
+    click(HOLD_REFERRAL_REASON_OPTION)
+    enter(note, HOLD_REFERRAL_NOTE)
+    click(HOLD_REFERRAL_BTN)
+    wait_for_spinner
   end
 
   def attach_document_to_referral(file_name:)
