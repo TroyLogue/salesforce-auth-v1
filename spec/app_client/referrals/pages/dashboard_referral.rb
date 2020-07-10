619a921698090129b9bd011995a43884c8a16882
Valerie Maldonado <valeriemaldonado@Valeries-MBP.home>
2020-07-10 16:02:42 -0400

    wip for rejected

--- spec/app_client/referrals/pages/dashboard_referral.rb
+++ spec/app_client/referrals/pages/dashboard_referral.rb
@@ -1,11 +1,20 @@
 require_relative '../../../../lib/file_helper'
 
 class Referral < BasePage
+  REFERRAL_STATUS = { css: '.detail-status-text.uppercase' }
+
   TAKE_ACTION_DROP_DOWN = { css: '.action-select-container' }
   TAKE_ACTION_OPTIONS = { css: 'div[data-value="send"]' }
+  TAKE_ACTION_REJECT_OPTION = { css: 'div[data-value="reject"]'}
 
   SENDER_INFO = { css: '#basic-table-sender-value' }
-  RECIPIENT_INFO = { css: '#basic-table-recipient-value'}
+  RECIPIENT_INFO = { css: '#basic-table-recipient-value' }
+
+  REJECT_REFERRAL_MODAL = { css: '.reject-modal-dialog .open' }
+  REJECT_REFERRAL_REASON_DROPDOWN = { css: '.referral-reject-display .referral-reason-field .choices__inner' }
+  REJECT_REFERRAL_REASON_OPTION = { css: '.referral-reason-field .choices.is-open #choices-reject-reason-input-item-choice-1' }
+  REJECT_REFERRAL_NOTE = { css: '#reject-note-input'}
+  REJECT_BTN = { css: '.referral-reject-display__reject-modal-form #reject-referral-reject-btn'}
 
   DOCUMENT_ADD_LINK = { css: '#upload-document-link' }
   DOCUMENT_ATTACH_MODAL = { css: '.dialog.open.large'}
@@ -20,6 +29,8 @@ class Referral < BasePage
   DOCUMENT_REMOVE_MODAL = { css: '.dialog.open.mini'}
   DOCUMENT_REMOVE_BTN = { css: '.confirmation-dialog__actions--confirm'}
 
+  REJECTED_STATUS = 'REJECTED'
+
   def go_to_new_referral_with_id(referral_id:)
     get("/dashboard/new/referrals/#{referral_id}")
     wait_for_spinner
@@ -27,6 +38,11 @@ class Referral < BasePage
 
   def go_to_send_referral_with_id(referral_id:)
     get("/dashboard/referrals/sent/all/#{referral_id}")
+    wait_for_spinner
+  end
+
+  def go_to_rejected_referral_with_id(referral_id:)
+    get("/dashboard/referrals/rejected/#{referral_id}")
     wait_for_spinner
   end
 
@@ -39,10 +55,26 @@ class Referral < BasePage
     text(RECIPIENT_INFO)
   end
 
+  def status
+    text(REFERRAL_STATUS)
+  end
+
   def send_referral_action
     click(TAKE_ACTION_DROP_DOWN)
     click(TAKE_ACTION_OPTIONS)
     wait_for_spinner
+  end
+
+  def reject_referral_action(note:)
+    click(TAKE_ACTION_DROP_DOWN)
+    click(TAKE_ACTION_REJECT_OPTION)
+    is_displayed?(REJECT_REFERRAL_MODAL)
+    click(REJECT_REFERRAL_REASON_DROPDOWN)
+    click(REJECT_REFERRAL_REASON_OPTION)
+    enter(note, REJECT_REFERRAL_NOTE)
+    click(REJECT_BTN)
+    wait_for_spinner
+    sleep(10)
   end
 
   def attach_document_to_referral(file_name:)
