410eecd29c209466dfe7483857c0042bae8728d1
Salman Hossain <salmanhin@gmail.com>
2020-08-05 16:59:53 -0400

    Resolve URI warning

--- spec/shared_components/base_page.rb
+++ spec/shared_components/base_page.rb
@@ -105,7 +105,8 @@ class BasePage
   end
 
   def get_uniteus_api_token
-    JSON.parse(URI.decode("#{driver.manage.cookie_named("uniteusApiToken")[:value]}"))["access_token"]
+    # should resolve warning: URI.unescape is obsolete
+    JSON.parse(URI.decode_www_form_component("#{driver.manage.cookie_named("uniteusApiToken")[:value]}"))["access_token"]
   end
 
   def get_uniteus_group
