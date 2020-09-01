568a5cf322dc5cf3da9d73c76ca57c8e38c922b4
Mo Shuheb <60902289+mgshuheb@users.noreply.github.com>
2020-09-01 10:49:46 -0400

    Update search_for_clients_spec.rb (#74)
    Co-authored-by: Mo Shuheb <60902289+mshuheb@users.noreply.github.com>

--- spec/app_client/search/search_for_clients_spec.rb
+++ spec/app_client/search/search_for_clients_spec.rb
@@ -28,7 +28,7 @@ describe '[Search]', :app_client, :search do
     it 'Search Page Results Table', :uuqa_663 do
       search_bar.go_to_search_results_page('E')
       expect(search_bar.are_results_not_displayed?).to be_truthy
-      expect(search_results_page.get_search_name_list).to all(start_with('E'))
+      expect(search_results_page.get_search_name_list).to all(include('E'))
     end
   end
 end
