235229414888483edbe47518f7c79a018f1a40b2
Leah D. Garber <leah.garber88@gmail.com>
2020-09-15 12:38:08 -0400

    support new pw reset flow

--- spec/app_client/auth/pages/login_email.rb
+++ spec/app_client/auth/pages/login_email.rb
@@ -8,6 +8,7 @@ class LoginEmail < BasePage
   NEXT_BUTTON = { css: 'input[value="Next"]' }
   PASSWORD_RESET_DIV = { css: '#flash_notice' }
   PASSWORD_RESET_TEXT = 'If your email address exists in our database, you will receive a password recovery link at your email address in a few minutes.'
+  PASSWORD_UPDATED_TEXT = 'New password set. You can now sign in with your new password.'
 
   def page_displayed?
     is_displayed?(AUTH_FORM) &&
@@ -17,6 +18,10 @@ class LoginEmail < BasePage
   def password_reset_message_displayed?
     is_displayed?(PASSWORD_RESET_DIV) &&
     text_include?(PASSWORD_RESET_TEXT, PASSWORD_RESET_DIV)
+  end
+
+  def password_updated_message_displayed?
+    text(AUTH_FORM).include?(PASSWORD_UPDATED_TEXT)
   end
 
   def submit(address)
--- spec/app_client/auth/reset_password_spec.rb
+++ spec/app_client/auth/reset_password_spec.rb
@@ -2,6 +2,7 @@ require_relative '../../../lib/mailtrap_helper'
 require_relative '../auth/helpers/login'
 require_relative '../auth/pages/forgot_password'
 require_relative '../auth/pages/reset_password'
+require_relative '../auth/pages/user_edit_password'
 require_relative '../root/pages/home_page'
 require_relative '../root/pages/notifications'
 require_relative '../root/pages/right_nav'
@@ -18,9 +19,11 @@ describe '[Auth - Reset Password]', :app_client, :auth, :order => :defined do
   let(:login_password) { LoginPassword.new(@driver) }
   let(:notifications) { Notifications.new(@driver) }
   let(:reset_password) { ResetPassword.new(@driver) }
+  let(:user_edit_password) { UserEditPassword.new(@driver) }
   let(:user_menu) { RightNav::UserMenu.new(@driver) }
   let(:user_settings) { UserSettings.new(@driver) }
 
+=begin
   context('[as cc user] From login page,') do
     let(:email) { Login::CC_HARVARD }
 
@@ -58,9 +61,11 @@ describe '[Auth - Reset Password]', :app_client, :auth, :order => :defined do
       expect(login_email.page_displayed?).to be_truthy
     end
   end
-
+=end
   context('[as org user] From user settings page,') do
     let (:reset_user) { Login::RESET_PW_USER }
+    let (:original_pw) { Login::DEFAULT_PASSWORD }
+    let (:insecure_pw) { Login::INSECURE_PASSWORD }
     let (:new_pw) { 'Uniteus123' }
 
     it 'cancels password reset while logged in', :uuqa_1493 do
@@ -69,12 +74,12 @@ describe '[Auth - Reset Password]', :app_client, :auth, :order => :defined do
       user_menu.go_to_user_settings
       expect(user_settings.page_displayed?).to be_truthy
 
-      # user_settings.click_reset_pw
-      # expect (reset_password.page_displayed?).to be_truthy
+      user_settings.click_reset_pw
+      expect(user_edit_password.page_displayed?).to be_truthy
 
-      # forgot_password.cancel_password_reset
+      user_edit_password.cancel_password_reset
       # should be back on the user settings page:
-      # expect(user_settings.page_displayed?).to be_truthy
+      expect(user_settings.page_displayed?).to be_truthy
     end
 
     it 'cannot reset password to an insecure password', :uuqa_803 do
@@ -83,13 +88,12 @@ describe '[Auth - Reset Password]', :app_client, :auth, :order => :defined do
       user_menu.go_to_user_settings
       expect(user_settings.page_displayed?).to be_truthy
 
-      # user_settings.click_reset_pw
-      # expect (reset_password.page_displayed?).to be_truthy
+      user_settings.click_reset_pw
+      expect(user_edit_password.page_displayed?).to be_truthy
 
-      # update the below:
-      user_settings.change_password(Login::INSECURE_PASSWORD)
-      notification_text = notifications.error_text
-      expect(notification_text).to include(Notifications::INSECURE_PASSWORD)
+      user_edit_password.update_password(original_pw, insecure_pw)
+      expect(user_edit_password.page_displayed?).to be_truthy
+      expect(user_edit_password.insecure_pw_message_displayed?).to be_truthy
     end
 
     it 'can reset password to a secure password', :uuqa_247 do
@@ -98,13 +102,14 @@ describe '[Auth - Reset Password]', :app_client, :auth, :order => :defined do
       user_menu.go_to_user_settings
       expect(user_settings.page_displayed?).to be_truthy
 
-      # user_settings.click_reset_pw
-      # expect (reset_password.page_displayed?).to be_truthy
+      user_settings.click_reset_pw
+      expect(user_edit_password.page_displayed?).to be_truthy
 
-      # change the below:
-      user_settings.change_password(new_pw)
-      notification_text = notifications.success_text
-      expect(notification_text).to include(Notifications::USER_UPDATED)
+      user_edit_password.update_password(original_pw, new_pw)
+
+      # verify success:
+      expect(login_email.page_displayed?).to be_truthy
+      expect(login_email.password_updated_message_displayed?).to be_truthy
     end
 
     #TODO - convert to API call for cleanup and remove :order => :defined tag
@@ -112,13 +117,14 @@ describe '[Auth - Reset Password]', :app_client, :auth, :order => :defined do
       log_in_as(reset_user, new_pw)
       expect(home_page.page_displayed?).to be_truthy #checking for a successful login
       user_menu.go_to_user_settings
-      # user_settings.click_reset_pw
-      # expect (reset_password.page_displayed?).to be_truthy
+      user_settings.click_reset_pw
+      expect(user_edit_password.page_displayed?).to be_truthy
 
-      # change the below:
-      user_settings.change_password(Login::DEFAULT_PASSWORD)
-      notification_text = notifications.success_text
-      expect(notification_text).to include(Notifications::USER_UPDATED)
+      user_edit_password.update_password(new_pw, original_pw)
+
+      # verify success:
+      expect(login_email.page_displayed?).to be_truthy
+      expect(login_email.password_updated_message_displayed?).to be_truthy
     end
   end
 end
