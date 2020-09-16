e4f07bb40af477cac9531e998ffa97e83148ce52
Bina Lati <sabina.dovlati@uniteus.com>
2020-09-16 16:18:14 -0400

    deleted new locator for submit_button and merged master

--- spec/app_client/auth/pages/login_password.rb
+++ spec/app_client/auth/pages/login_password.rb
@@ -1,17 +1,16 @@
 require_relative '../../../shared_components/base_page'
 
 class LoginPassword < BasePage
-  FORGOT_PASSWORD_LINK = { css: '#forgot-password-link' }
-  INVALID_ALERT = { css: '#flash_alert' }
-  INVALID_TEXT = 'Invalid Email or password.'
-  NOT_YOU_LINK = { css: '#not-you-link' }
-  PASSWORD_INPUT = { css: '#app_2_user_password' }
-  # SUBMIT_BUTTON = { css: 'input[value="Sign In"]' }
-  SUBMIT_BUTTON = { name: 'commit' }
-  USER_EMAIL = { css: '#user-email' }
+  FORGOT_PASSWORD_LINK = { css: '#forgot-password-link' }.freeze
+  INVALID_ALERT = { css: '#flash_alert' }.freeze
+  INVALID_TEXT = 'Invalid Email or password.'.freeze
+  NOT_YOU_LINK = { css: '#not-you-link' }.freeze
+  PASSWORD_INPUT = { css: '#app_2_user_password' }.freeze
+  SUBMIT_BUTTON = { css: 'input[value="Sign In"]' }.freeze
+  USER_EMAIL = { css: '#user-email' }.freeze
 
   # string to pass to enter_via_js which uses getElementById
-  PASSWORD_INPUT_ID = 'app_2_user_password'
+  PASSWORD_INPUT_ID = 'app_2_user_password'.freeze
 
   def click_forgot_password
     click_via_js(FORGOT_PASSWORD_LINK)
@@ -23,14 +22,14 @@ class LoginPassword < BasePage
 
   def invalid_alert_text
     find(INVALID_ALERT)
-    return text(INVALID_ALERT)
+    text(INVALID_ALERT)
   end
 
   def page_displayed?
     is_displayed?(FORGOT_PASSWORD_LINK) &&
-    is_displayed?(NOT_YOU_LINK) &&
-    is_displayed?(USER_EMAIL) &&
-    is_displayed?(PASSWORD_INPUT)
+      is_displayed?(NOT_YOU_LINK) &&
+      is_displayed?(USER_EMAIL) &&
+      is_displayed?(PASSWORD_INPUT)
   end
 
   def submit(password)
