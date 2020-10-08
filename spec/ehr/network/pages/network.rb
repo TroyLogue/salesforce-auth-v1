eabf9470575ea49bb09359e09252e2ba2377bdab
Leah D. Garber <leah.garber88@gmail.com>
2020-10-08 18:37:34 -0400

    remove network changes from this branch

--- spec/ehr/network/pages/network.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../../../shared_components/base_page'
-
-class Network < BasePage
-  INDEX_EHR = { css: '.network-directory-index' }
-
-  def page_displayed?
-    is_displayed?(INDEX_EHR)
-  end
-end
