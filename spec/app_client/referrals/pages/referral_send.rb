5edb5c4ffd52b60ec220d0714c00f3f3ee49ca06
Carol Mirakove <carol@uniteus.com>
2020-10-08 10:12:50 -0400

    strip distance from provider before asserting text (#103)
    * strip distance from provider before asserting text
    
    * account for providers with parens in their name

--- spec/app_client/referrals/pages/referral_send.rb
+++ spec/app_client/referrals/pages/referral_send.rb
@@ -1,16 +1,16 @@
 require_relative '../../../../lib/file_helper'
 
 class ReferralSend < BasePage
-  SEND_REFERRAL_FORM = { css: '.send-referral-form' }
+  SEND_REFERRAL_FORM = { css: '.send-referral-form' }.freeze
 
-  SUGGESTED_ORG_SECTION = { css: '.referral-groups-programs' }
-  BROWSE_MAP_LINK = { css: '#browse-map-button' }
-  SELECTED_ORG_DROPDOWN = { css: '.referral-group-select div[aria-selected="true"]' }
-  SEND_REFERRAL_BTN = { css: '#send-referral-send-btn' }
+  SUGGESTED_ORG_SECTION = { css: '.referral-groups-programs' }.freeze
+  BROWSE_MAP_LINK = { css: '#browse-map-button' }.freeze
+  SELECTED_ORG_DROPDOWN = { css: '.referral-group-select div[aria-selected="true"]' }.freeze
+  SEND_REFERRAL_BTN = { css: '#send-referral-send-btn' }.freeze
 
   def page_displayed?
     is_displayed?(SEND_REFERRAL_FORM) &&
-    is_displayed?(SUGGESTED_ORG_SECTION)
+      is_displayed?(SUGGESTED_ORG_SECTION)
   end
 
   def open_network_browse_map
@@ -18,8 +18,10 @@ class ReferralSend < BasePage
   end
 
   def selected_organization
-    # Removing excess text
-    text(SELECTED_ORG_DROPDOWN).gsub("\nRemove item", "")
+    # Removing distance and "Remove Item" to return just the provider name
+    provider_text = text(SELECTED_ORG_DROPDOWN)
+    provider_distance_index = text(SELECTED_ORG_DROPDOWN).rindex(/\(/) # finds the last open paren in the string
+    provider_text[0..(provider_distance_index - 1)].strip # returns provider_text up to the distance
   end
 
   def send_referral
