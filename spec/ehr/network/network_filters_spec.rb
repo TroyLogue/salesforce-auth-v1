e0eba03eb4a87aee307e814fee73f129f077d197
Leah D. Garber <leah.garber88@gmail.com>
2020-11-03 08:25:32 -0800

    make instance variables local

--- spec/ehr/network/network_filters_spec.rb
+++ spec/ehr/network/network_filters_spec.rb
@@ -23,21 +23,21 @@ describe '[Network]', :ehr, :network do
     }
 
     it 'can filter by text', :uuqa_1548 do
-      @provider_search_text = "Princeton"
+      provider_search_text = "Princeton"
       # TODO: https://uniteus.atlassian.net/browse/UU3-48920
       # replace hardcoded search term with provider name from API
-      network.search_by_text(text: @provider_search_text)
+      network.search_by_text(text: provider_search_text)
       # verify at least one result:
       expect(network.search_result_text).to include("result")
-      expect(network.first_provider_name).to include(@provider_search_text)
+      expect(network.first_provider_name).to include(provider_search_text)
     end
 
     it 'can filter by service type, distance, and address', :uuqa_1558 do
       aggregate_failures 'different filter options' do
         # filter by service type
-        @service_type = "Disability Benefits"
-        network.select_service_type(@service_type)
-        expect(network.search_result_text).to include(@service_type)
+        service_type = "Disability Benefits"
+        network.select_service_type(service_type)
+        expect(network.search_result_text).to include(service_type)
 
         # open filter drawer
         network.open_filter_drawer
@@ -45,14 +45,14 @@ describe '[Network]', :ehr, :network do
         expect(filter_drawer.page_displayed?).to be_truthy
 
         # filter by distance
-        @distance = "25 Miles"
-        filter_drawer.filter_distance_by_miles(@distance)
-        expect(network.search_result_text).to include(@distance.downcase);
+        distance = "25 Miles"
+        filter_drawer.filter_distance_by_miles(distance)
+        expect(network.search_result_text).to include(distance.downcase);
 
         # filter by address
-        @nyc_office_address= "217 Broadway, New York, NY 10007, USA"
-        filter_drawer.submit_other_address(@nyc_office_address)
-        expect(network.search_result_text).to include(@nyc_office_address)
+        nyc_office_address= "217 Broadway, New York, NY 10007, USA"
+        filter_drawer.submit_other_address(nyc_office_address)
+        expect(network.search_result_text).to include(nyc_office_address)
 
         # close filter drawer
         filter_drawer.close_drawer
