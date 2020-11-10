63c3e068266ed3ddb5f17bf7265e9b86add31627
Leah D. Garber <leah.garber88@gmail.com>
2020-11-10 12:53:13 -0800

    small refactor to match referrals ehr structure

--- spec/ehr/assessments/edit_assessment_spec.rb
+++ spec/ehr/assessments/edit_assessment_spec.rb
@@ -1,12 +1,14 @@
 require_relative '../auth/helpers/login_ehr'
 require_relative '../root/pages/home_page'
-require_relative './pages/assessment_page'
+require_relative './pages/assessment'
+require_relative './pages/assessments_table'
 require_relative '../root/pages/notifications_ehr'
 
 describe '[Assessments]', :ehr, :assessments do
   include LoginEhr
 
-  let(:assessment_page) { AssessmentPage.new(@driver) }
+  let(:assessment) { AssessmentPage.new(@driver) }
+  let(:assessments_table) { AssessmentsTable.new(@driver) }
   let(:homepage) { HomePage.new(@driver) }
   let(:login_email_ehr) { LoginEmailEhr.new(@driver) }
   let(:login_password_ehr) { LoginPasswordEhr.new(@driver) }
@@ -20,21 +22,21 @@ describe '[Assessments]', :ehr, :assessments do
     }
 
     it 'can edit an assessment', :uuqa_399 do
-      assessment = "Ivy League General Form"
+      assessment_name = "Ivy League General Form"
       new_value = Faker::Alphanumeric.alpha(number: 8)
 
-      homepage.open_assessment_by_name(assessment)
-      expect(assessment_page.page_displayed?(assessment_name: assessment)).to be_truthy
+      assessments_table.open_assessment_by_name(assessment_name)
+      expect(assessment.page_displayed?(assessment_name: assessment_name)).to be_truthy
 
-      initial_value = assessment_page.first_response_text
-      assessment_page.edit_assessment(new_value)
+      initial_value = assessment.first_response_text
+      assessment.edit_assessment(new_value)
 
       # verify success notification:
       notification_text = notifications.success_text
       expect(notification_text).to include(NotificationsEhr::ASSESSMENT_UPDATED)
 
       # verify value updated:
-      updated_value = assessment_page.first_response_text
+      updated_value = assessment.first_response_text
       expect(updated_value).not_to include(initial_value)
       expect(updated_value).to include(new_value)
     end
