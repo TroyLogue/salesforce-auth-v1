7eb160497d606955a0d0575294c8ad8133c01f20
Valerie Maldonado <44375279+mvalerie23@users.noreply.github.com>
2020-11-11 14:20:49 -0500

    removing port (#129)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -88,7 +88,6 @@ RSpec.configure do |config|
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
-        options.add_argument('--remote-debugging-port=9222')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--window-size=1280,720')
         options.add_argument('—-disk-cache-size=0')
