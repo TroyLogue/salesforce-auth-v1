4e0ca004647aba94566574bb7d01fb3328152272
Carol Mirakove <carol@uniteus.com>
2020-11-20 16:33:27 -0500

    CORE-284 2FA (#137)
    * CORE-257 update reset password flow
    
    * update locator to use data-qa
    
    * CORE-284 submit two-factor-auth
    
    * fix typo

--- spec/app_client/user_settings/pages/security_settings_page.rb
+++ spec/app_client/user_settings/pages/security_settings_page.rb
@@ -5,18 +5,17 @@ require_relative '../../../shared_components/base_page'
 module UserSettings
   class SecuritySettingsPage < BasePage
     AUTH_FORM = { id: 'auth-form-container' }.freeze
+    CHANGE_PW_LINK = { css: 'a[href="/users/edit"]' }.freeze
     TWO_FACTOR_AUTH_ENABLE_LINK = { css: 'a[href="/users/two_factor_registration/new"]' }.freeze
-    RESET_PW_LINK = { css: 'a[href="/users/edit"]' }.freeze
     USER_SETTINGS_LINK = { css: 'a[href$="/user/settings"]' }.freeze
 
     def load_page
-      get_auth('/users/settings')
+      get_auth('/users/security-settings')
     end
 
     def page_displayed?
       is_displayed?(AUTH_FORM) &&
-        is_displayed?(TWO_FACTOR_AUTH_ENABLE_LINK) &&
-        is_displayed?(RESET_PW_LINK) &&
+        is_displayed?(CHANGE_PW_LINK) &&
         is_displayed?(USER_SETTINGS_LINK)
     end
 
@@ -30,6 +29,10 @@ module UserSettings
 
     def click_two_factor_auth
       click(TWO_FACTOR_AUTH_ENABLE_LINK)
+    end
+
+    def turn_on_two_factor_auth_link_displayed?
+      is_displayed?(TWO_FACTOR_AUTH_ENABLE_LINK)
     end
   end
 end
---
+++ spec/app_client/user_settings/pages/two_factor_auth_new_page.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative '../../../shared_components/base_page'
+
+module UserSettings
+  class TwoFactorAuthNewPage < BasePage
+    CANCEL_LINK = { css: '.cancel-link' }.freeze
+    PHONE_NUMBER_INPUT = { name: 'two_factor_registration[phone_number]' }.freeze
+    SEND_CODE_BUTTON = { css: "input[value='Send Code']" }.freeze
+
+    # For the purposes of this blackbox test we can use any U.S. phone number;
+    # For integration tests with Twilio, see references to their magic numbers:
+    # https://www.twilio.com/blog/2018/04/twilio-test-credentials-magic-numbers.html
+    # https://www.twilio.com/docs/iam/test-credentials
+    VALID_TEST_PHONE_NUMBER = '5005550006'
+
+    def load_page
+      get_auth('/users/two_factor_registration/new')
+    end
+
+    def page_displayed?
+      is_displayed?(PHONE_NUMBER_INPUT) &&
+        is_displayed?(SEND_CODE_BUTTON) &&
+        is_displayed?(CANCEL_LINK)
+    end
+
+    def submit_phone_number(number:)
+      enter(number, PHONE_NUMBER_INPUT)
+      click(SEND_CODE_BUTTON)
+    end
+  end
+end
