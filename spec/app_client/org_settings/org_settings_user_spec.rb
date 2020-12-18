07f8c578cdd2432da4850137bd6b5a83eb4e8c19
Carol Mirakove <carol@uniteus.com>
2020-12-18 15:37:01 -0500

    CORE-372 UUQA-355 remove hard-coding; update css for core (#152)
    * CORE-372 UUQA-355 remove hard-coding; update css for core
    
    * replace sleeps with waits
    
    * add comments on waits

--- spec/app_client/org_settings/org_settings_user_spec.rb
+++ spec/app_client/org_settings/org_settings_user_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../auth/helpers/login'
+require_relative '../root/pages/notifications'
 require_relative '../root/pages/right_nav'
 require_relative './pages/org_settings_user_page'
 
@@ -9,6 +10,7 @@ describe '[Org Settings - Users]', :org_settings, :app_client do
 
   let(:login_email) { LoginEmail.new(@driver) }
   let(:login_password) { LoginPassword.new(@driver) }
+  let(:notifications) { Notifications.new(@driver) }
   let(:org_menu) { RightNav::OrgMenu.new(@driver) }
   let(:org_settings_user_table) { OrgSettings::UserTable.new(@driver) }
   let(:org_settings_user_form) { OrgSettings::UserCard.new(@driver) }
@@ -31,10 +33,12 @@ describe '[Org Settings - Users]', :org_settings, :app_client do
     end
 
     it 'can view and edit existing user form', :uuqa_355 do
-      org_settings_user_table.go_to_user(name: 'Permissions, QA')
-      expect(org_settings_user_form.get_user_title).to eql("QA Permissions's Profile")
+      org_settings_user_table.go_to_first_user
       expect(org_settings_user_form.existing_user_fields_editable?).to be_truthy
-      expect(org_settings_user_form.save_email_field?).to be_truthy
+      org_settings_user_form.save_email_field
+
+      notification_text = notifications.success_text
+      expect(notification_text).to eql Notifications::USER_UPDATED
     end
   end
 end
