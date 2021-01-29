ec2d55ea9cbc74fa087b3c4bf2f0446835cab750
Erik Belusic <erikbelusic@users.noreply.github.com>
2021-01-29 11:43:01 -0500

    cases - filters by primary worker

---
+++ spec/app_client/cases/filter_cases_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative '../auth/helpers/login'
+require_relative '../root/pages/home_page'
+require_relative '../cases/pages/open_cases_dashboard'
+require_relative '../root/pages/notifications'
+
+describe '[cases]', :app_client, :cases do
+  include Login
+
+  let(:login_email) { LoginEmail.new(@driver) }
+  let(:login_password) { LoginPassword.new(@driver) }
+  let(:homepage) { HomePage.new(@driver) }
+  let(:open_cases_dashboard) { OpenCasesDashboard.new(@driver) }
+  let(:notifications) { Notifications.new(@driver) }
+
+  context('[as non cc user]') do
+    before(:each) do
+      log_in_as(Login::ORG_YALE)
+      expect(homepage.page_displayed?).to be_truthy
+    end
+
+    it 'works', :uuqa_1713 do
+      open_cases_dashboard.go_to_open_cases_dashboard
+      expect(open_cases_dashboard.open_cases_table_displayed?).to be_truthy
+
+      primary_worker = open_cases_dashboard.search_and_select_first_primary_worker('e')
+      # primary_worker = open_cases_dashboard.select_first_primary_worker
+
+      expect(notifications.notification_not_displayed?).to be_truthy
+      expect(open_cases_dashboard.no_cases_message_displayed? || open_cases_dashboard.cases_match_primary_worker?(primary_worker)).to be_truthy
+    end
+  end
+end
--- spec/app_client/cases/pages/open_cases_dashboard.rb
+++ spec/app_client/cases/pages/open_cases_dashboard.rb
@@ -2,8 +2,37 @@ require_relative '../../../shared_components/base_page'
 
 class OpenCasesDashboard < BasePage
   OPEN_CASES_TABLE = { css: '.dashboard-content #open-cases-table' }.freeze
+  PRIMARY_WORKER_DROPDOWN = { css: '#primary-worker-filter' }.freeze
+  PRIMARY_WORKER_SEARCH = { css: '#primary-worker-filter .ui-filter-search > input' }.freeze
+  PRIMARY_WORKER_FIRST_OPTION = { css: '#primary-worker-filter .filter-options__container div:nth-of-type(2)' }.freeze
+  PRIMARY_WORKER_LIST = { css: '#open-cases-table > .ui-table > .ui-table-body > tbody > tr > td:nth-of-type(5)' }.freeze
+  NO_CASES_TEXT_CONTAINER = { css: '#open-cases-table .dashboard-inner-content > div > div > h3' }.freeze
+  NO_CASES_TEXT = 'There are no open cases.'
 
   def open_cases_table_displayed?
     is_displayed?(OPEN_CASES_TABLE)
+  end
+
+  def go_to_open_cases_dashboard
+    get('/dashboard/cases/open')
+  end
+
+  def search_and_select_first_primary_worker(text)
+    click(PRIMARY_WORKER_DROPDOWN)
+    clear_then_enter(text, PRIMARY_WORKER_SEARCH)
+    is_displayed?(PRIMARY_WORKER_FIRST_OPTION)
+    selected_option_text = text({ css: "#{PRIMARY_WORKER_FIRST_OPTION[:css]} label" })
+    click(PRIMARY_WORKER_FIRST_OPTION)
+    click(PRIMARY_WORKER_DROPDOWN)
+    selected_option_text
+  end
+
+  def no_cases_message_displayed?
+    is_displayed?(NO_CASES_TEXT_CONTAINER) && text(NO_CASES_TEXT_CONTAINER).eql?(NO_CASES_TEXT)
+  end
+
+  def cases_match_primary_worker?(text)
+    primary_workers = find_elements(PRIMARY_WORKER_LIST).map(&:text)
+    primary_workers.all? {|pm| pm.eql?(text)}
   end
 end
