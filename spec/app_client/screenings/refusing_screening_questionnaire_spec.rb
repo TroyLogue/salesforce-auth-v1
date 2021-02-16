268d719f106e63eb7891362c4f790e9e2e66c4fe
tonydeng <dev.tonybd@gmail.com>
2021-02-16 14:20:05 -0500

    UU3-51322

---
+++ spec/app_client/screenings/refusing_screening_questionnaire_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require_relative '../auth/helpers/login'
+require_relative '../root/pages/home_page'
+require_relative '../root/pages/dashboard_nav'
+require_relative '../facesheet/pages/facesheet_header'
+require_relative '../facesheet/pages/facesheet_overview_page'
+require_relative '../facesheet/pages/facesheet_forms_page'
+require_relative './pages/new_screening_page'
+
+describe '[Screenings - Refuse Screening]', :screenings, :app_client do
+  include Login
+
+  let(:login_email) { LoginEmail.new(@driver) }
+  let(:login_password) { LoginPassword.new(@driver) }
+  let(:homepage) { HomePage.new(@driver) }
+  let(:left_nav) { LeftNav.new(@driver) }
+  let(:clients_page) { ClientsPage.new(@driver)}
+  let(:facesheet_overview_page) { FacesheetOverviewPage.new(@driver) }
+  let(:facesheet_forms) { FacesheetForms.new(@driver) }
+  let(:facesheet_header) { FacesheetHeader.new(@driver) }
+  let(:new_screening_page) { NewScreeningPage.new(@driver) }
+
+  context('[As a user with screening role]') do
+    before {
+      log_in_as(Login::ORG_NEWYORK)
+      expect(homepage.page_displayed?).to be_truthy
+      left_nav.go_to_clients
+      clients_page.go_to_facesheet_random_authorized_client
+      expect(facesheet_overview_page.page_displayed?).to be_truthy
+      expect(facesheet_header.page_displayed?).to be_truthy
+      facesheet_header.go_to_forms
+    }
+
+    it "No need for resources message is displayed when screening is refused by a client", :uuqa_1755 do
+      facesheet_forms.create_new_screening
+      new_screening_page.refuse_to_fill_our_screening
+      expect(new_screening_page.no_resources_needed_message_displayed?).to be true
+    end
+  end
+end
