a8902abe1a58459500d9c5c7913be5ad04e3099b
Leah D. Garber <leah.garber88@gmail.com>
2021-03-18 12:00:41 -0400

    clean up spec names and setup

--- spec/app_client/messaging/accept_referral_email_notifications_spec.rb
+++ spec/app_client/messaging/accept_referral_email_notifications_spec.rb
@@ -2,20 +2,18 @@ describe '[Messaging - Email Notifications]', :app_client, :messaging do
   include Login
   include MailtrapHelper
 
-  context('[as an org user]') do
+  context('[as an org user] When I accept a referral for a Sensitive service type') do
     before {
       # Create contact with email notifications enabled
       @contact = Setup::Data.create_harvard_client_with_consent
       Setup::Data.enable_email_notifications_for_contact(contact: @contact)
 
-      # Create sensitive referral and send to Princeton
+      # Create sensitive referral and accept in Princeton
       @sensitive_referral = Setup::Data.send_sensitive_referral_from_harvard_to_princeton(contact_id: @contact.contact_id);
-
-      # Accept the referral in Princeton
       Setup::Data.accept_referral_in_princeton
     }
 
-    it 'receives an email when a sensitive referral is accepted', :uuqa_1467 do
+    it 'client receives an email that does not include the service type name', :uuqa_1467 do
       # Check mailtrap
       message = get_first_message(filter: '[STAGING] Your referral has been accepted')
       # Verify email body contains correct text
@@ -25,20 +23,18 @@ describe '[Messaging - Email Notifications]', :app_client, :messaging do
     end
   end
 
-  context('[as an org user] Non-sensitive referral') do
+  context('[as an org user] When I accept a referral for a Non-sensitive service type') do
     before {
       # Create contact with email notifications enabled
       @contact = Setup::Data.create_harvard_client_with_consent
       Setup::Data.enable_email_notifications_for_contact(contact: @contact)
 
-      # Create sensitive referral and send to Princeton
+      # Create non-sensitive referral and accept in Princeton
       @referral = Setup::Data.send_referral_from_harvard_to_princeton(contact_id: @contact.contact_id);
-
-      # Accept the referral in Princeton
       Setup::Data.accept_referral_in_princeton
     }
 
-    it 'receives an email when a non-sensitive referral is accepted', :uuqa_1467 do
+    it 'client receives an email that includes the service type name', :uuqa_1467 do
       message = get_first_message(filter: '[STAGING] Your Disability Benefits referral has been accepted')
       expect(body(message)).to include('Your Disability Benefits referral has been accepted and you should expect to be contacted.')
     end
