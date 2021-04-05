3f8df3f0fa2cb691a1f4b96dc93317025cb9ab70
Leah D. Garber <leah.garber88@gmail.com>
2021-04-05 13:10:15 -0400

    move form_data to private method

--- spec/support/setup/base/screenings.rb
+++ spec/support/setup/base/screenings.rb
@@ -1,28 +1,6 @@
 require 'uniteus-api-client'
 require 'faker'
 
-def form_data(questions)
-  data = []
-  questions.map do |question|
-    case question.input_type
-    when 'select'
-      data.push({ question_id: question.id, response_value: [question.input_options.sample.id] })
-    when 'number'
-      data.push({ question_id: question.id, response_value: rand(1..3) })
-    when 'email'
-      data.push({ question_id: question.id, response_value: Faker::Internet.email })
-    when 'date'
-      data.push({ question_id: question.id, response_value: (DateTime.now - 1).to_time.to_i })
-    when 'duration'
-      data.push({ question_id: question.id, response_value: [(DateTime.now - 2).to_time.to_i, (DateTime.now - 1).to_time.to_i] })
-    else
-      data.push({ question_id: question.id, response_value: Faker::Lorem.sentence })
-    end
-  end
-
-  data
-end
-
 module Setup
   class Screenings
     class << self
@@ -46,6 +24,30 @@ module Setup
         raise("Response returned: #{response.status}") unless response.status == 201
 
         JSON.parse(response, object_class: OpenStruct).data
+      end
+
+      private
+
+      def form_data(questions)
+        data = []
+        questions.map do |question|
+          case question.input_type
+          when 'select'
+            data.push({ question_id: question.id, response_value: [question.input_options.sample.id] })
+          when 'number'
+            data.push({ question_id: question.id, response_value: rand(1..3) })
+          when 'email'
+            data.push({ question_id: question.id, response_value: Faker::Internet.email })
+          when 'date'
+            data.push({ question_id: question.id, response_value: (DateTime.now - 1).to_time.to_i })
+          when 'duration'
+            data.push({ question_id: question.id, response_value: [(DateTime.now - 2).to_time.to_i, (DateTime.now - 1).to_time.to_i] })
+          else
+            data.push({ question_id: question.id, response_value: Faker::Lorem.sentence })
+          end
+        end
+
+        data
       end
     end
   end
