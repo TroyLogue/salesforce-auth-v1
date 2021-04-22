d20c649b45779702deacae64d1493479d559af91
Leah D. Garber <leah.garber88@gmail.com>
2021-04-22 11:21:02 -0400

    set staging and devqa as defined cases

--- spec/support/setup/identifiers/network_ids.rb
+++ spec/support/setup/identifiers/network_ids.rb
@@ -7,7 +7,11 @@ module Networks
   when 'training'
     # just for debugging - not actually Ivy in Training
     IVY = '0c96386c-ed27-4c9b-bb90-6544701865eb'
-  else
+  when 'staging'
     IVY = 'd8e7a7f0-d4f1-459b-bab4-15802b371cae'
+  when 'devqa'
+    IVY = 'd8e7a7f0-d4f1-459b-bab4-15802b371cae'
+  else
+    raise "Missing required ENV['environment']: prod, training, staging, devqa"
   end
 end
