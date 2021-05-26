830bee72f0186d231c34bbc1c033e0da02bb0700
Carol Mirakove <carol@uniteus.com>
2021-05-26 16:09:50 -0400

    CORE-868 reports, screenings (#302)
    * CORE-868 reports
    
    * CORE-868 screenings

--- spec/support/setup/identifiers/users.rb
+++ spec/support/setup/identifiers/users.rb
@@ -12,9 +12,12 @@ module Users
     CC_QA_ADMIN = 'qa.perms@auto.com'
     ORG_NO_ADDRESS = 'chicago@auto.com'
     ORG_PRINCETON = 'princeton@auto.com'
+    ORG_WITH_SCREENING_ROLE = 'newyork@auto.com'
+    ORG_WITHOUT_SCREENING_ROLE = 'princeton@auto.com'
     ORG_YALE = 'yale@auto.com'
     ORG_COLUMBIA = 'columbia@auto.com'
     ORG_NEWYORK = 'newyork@auto.com'
+    SCREENINGS_USER_MULTI_NETWORK = 'screenings@emr.com'
     DEFAULT_PASSWORD = ENV['DEFAULT_PASSWORD']
   else
     raise "Missing required ENV['environment']: prod, training, staging, devqa"
