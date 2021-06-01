6bf44d65c7f647e9b1a4a709449d6721fd82e6e8
Carol Mirakove <carol@uniteus.com>
2021-06-01 10:38:15 -0400

    debugging for intermittent failures (#300)

--- spec/app_client/org_settings/users_spec.rb
+++ spec/app_client/org_settings/users_spec.rb
@@ -108,6 +108,9 @@ describe '[Org Settings - Users]', :org_settings, :app_client do
 
       removed_value = org_settings_user_form.remove_org_role
       org_access_values.delete(removed_value)
+      # adding debugging to rule out functional bug vs. race condition in banner
+      # reference: https://uniteus.atlassian.net/wiki/spaces/QA/pages/2597945489/5+24+2021+-+5+28+2021#end-to-end-tests
+      p "E2E DEBUG: removed_value is #{removed_value}, org_access_values are #{org_access_values}"
       expect(notifications.success_text).to include(Notifications::USER_UPDATED)
       expect(org_settings_user_form.displayed_program_access_values[:org_role_values])
         .to match_array(org_access_values)
