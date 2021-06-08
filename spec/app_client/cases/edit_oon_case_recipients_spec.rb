7a4e851a9dbb3a1aabeb645dd16bb9004640262c
Leah Garber <leah.garber88@gmail.com>
2021-06-08 10:17:31 -0400

    replace setup, match on array without accounting for order

--- spec/app_client/cases/edit_oon_case_recipients_spec.rb
+++ spec/app_client/cases/edit_oon_case_recipients_spec.rb
@@ -11,9 +11,9 @@ describe '[Cases]', :app_client, :cases do
 
   context('[as an employee with Out of Network user role]') do
     before do
-#      @contact = Setup::Data.create_harvard_client_with_consent
+      @contact = Setup::Data.create_harvard_client_with_consent
 
-#      @case = Setup::Data.create_oon_case_for_harvard(contact_id: @contact.contact_id)
+      @case = Setup::Data.create_oon_case_for_harvard(contact_id: @contact.contact_id)
 
       @auth_token = Auth.encoded_auth_token(email_address: Users::CC_USER)
       homepage.authenticate_and_navigate_to(token: @auth_token, path: '/')
@@ -21,8 +21,7 @@ describe '[Cases]', :app_client, :cases do
     end
 
      it 'can edit the referred-to values for an open OON case', :uuqa_1817 do
-#      case_detail_page.go_to_open_case_with_id(case_id: @case.id, contact_id: @contact.contact_id)
-      homepage.get('/dashboard/cases/open/3c057143-ebeb-4048-aec2-49ff4869714b/contact/981b1287-53e4-4963-90af-a6aa52cf509e')
+      case_detail_page.go_to_open_case_with_id(case_id: @case.id, contact_id: @contact.contact_id)
       expect(case_detail_page.page_displayed?).to be_truthy
       initial_referred_to = case_detail_page.referred_to
 
@@ -37,7 +36,7 @@ describe '[Cases]', :app_client, :cases do
       notification_text = notifications.success_text
       expect(notification_text).to include(Notifications::CASE_UPDATED)
 
-      expect(case_detail_page.referred_to_array).to eq([initial_referred_to, new_oon_recipient, new_custom_recipient])
+      expect(case_detail_page.referred_to_array).to match_array([initial_referred_to, new_oon_recipient, new_custom_recipient])
     end
   end
 end
