de299def6fccb9c78000042084815f6a0a7fd81e
Leah Garber <leah.garber88@gmail.com>
2021-06-11 10:55:55 -0400

    replacing missing selectors; add page displayed for closed case dashboard

--- spec/app_client/cases/pages/case_dashboard.rb
+++ spec/app_client/cases/pages/case_dashboard.rb
@@ -2,29 +2,15 @@ require_relative '../../../shared_components/base_page'
 
 module CaseDashboard
   module SharedComponents
-  end
-
-  class Open < BasePage
-    include SharedComponents
-
-    OPEN_CASES_TABLE = { css: '.dashboard-content #open-cases-table' }.freeze
+    CARE_COORDINATOR_DROPDOWN = { css: '#care-coordinator-filter' }.freeze
+    CARE_COORDINATOR_SEARCH = { css: '#care-coordinator-filter .ui-filter-search > input' }.freeze
+    CARE_COORDINATOR_FIRST_OPTION = { css: '#care-coordinator-filter .filter-options__container div:nth-of-type(2)' }.freeze
+    CARE_COORDINATOR_SELECTED_OPTION = { css: "#{CARE_COORDINATOR_FIRST_OPTION[:css]} label" }.freeze
 
     PRIMARY_WORKER_DROPDOWN = { css: '#primary-worker-filter' }.freeze
     PRIMARY_WORKER_SEARCH = { css: '#primary-worker-filter .ui-filter-search > input' }.freeze
     PRIMARY_WORKER_FIRST_OPTION = { css: '#primary-worker-filter .filter-options__container div:nth-of-type(2)' }.freeze
     PRIMARY_WORKER_SELECTED_OPTION = { css: "#{PRIMARY_WORKER_FIRST_OPTION[:css]} label" }.freeze
-    PRIMARY_WORKER_LIST = { css: '#open-cases-table > .ui-table > .ui-table-body > tbody > tr > td:nth-of-type(5)' }.freeze
-
-    NO_CASES_TEXT_CONTAINER = { css: '#open-cases-table .dashboard-inner-content > div > div > h3' }.freeze
-    NO_CASES_TEXT = 'There are no open cases.'
-
-    def open_cases_table_displayed?
-      is_displayed?(OPEN_CASES_TABLE)
-    end
-
-    def go_to_open_cases_dashboard
-      get('/dashboard/cases/open')
-    end
 
     def search_and_select_first_primary_worker(text)
       click(PRIMARY_WORKER_DROPDOWN)
@@ -34,6 +20,24 @@ module CaseDashboard
       click(PRIMARY_WORKER_FIRST_OPTION)
       click(PRIMARY_WORKER_DROPDOWN)
       selected_option_text
+    end
+  end
+
+  class Open < BasePage
+    include SharedComponents
+
+    CARE_COORDINATOR_LIST = { css: '#open-cases-table > .ui-table > .ui-table-body > tbody > tr > td:nth-of-type(5)' }.freeze
+    NO_CASES_TEXT_CONTAINER = { css: '#open-cases-table .dashboard-inner-content > div > div > h3' }.freeze
+    NO_CASES_TEXT = 'There are no open cases.'
+    OPEN_CASES_TABLE = { css: '.dashboard-content #open-cases-table' }.freeze
+    PRIMARY_WORKER_LIST = { css: '#open-cases-table > .ui-table > .ui-table-body > tbody > tr > td:nth-of-type(5)' }.freeze
+
+    def open_cases_table_displayed?
+      is_displayed?(OPEN_CASES_TABLE)
+    end
+
+    def go_to_open_cases_dashboard
+      get('/dashboard/cases/open')
     end
 
     def search_and_select_first_care_coordinator(text)
@@ -64,8 +68,10 @@ module CaseDashboard
   class Closed < BasePage
     include SharedComponents
 
-    def page_displayed?
+    CLOSED_CASES_TABLE = { css: '#closed-cases-table' }.freeze
 
+    def page_displayed?
+      is_displayed?(CLOSED_CASES_TABLE)
     end
   end
 end
