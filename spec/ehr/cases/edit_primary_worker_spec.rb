d36536ddbd5b8120dfa947e1423a7bffa32e3d5b
Leah Garber <leah.garber88@gmail.com>
2021-09-29 09:41:43 -0400

    cases in progress

--- spec/ehr/cases/edit_primary_worker_spec.rb
+++ spec/ehr/cases/edit_primary_worker_spec.rb
@@ -23,14 +23,15 @@ describe '[Cases]', :ehr, :cases do
 
       log_in_dashboard_as(LoginEhr::CC_HARVARD)
       expect(home_page.page_displayed?).to be_truthy
+      session_support_id = home_page.ehr_session_support_id
 
       # click first case - just until EHR-525 is fixed
-      home_page.go_to_cases_tab
-      expect(cases_table.page_displayed?).to be_truthy
-      cases_table.click_first_case
+#      home_page.go_to_cases_tab
+#      expect(cases_table.page_displayed?).to be_truthy
+#      cases_table.click_first_case
 
       # go to detail page of case created above; requires session_support_id to build EHR URL
-#      base_page.get("/#{session_support_id}/2/patient/#{@case.contact.id}/cases/#{@case.id}")
+      base_page.get("/#{session_support_id}/2/patient/#{@case.contact.id}/cases/#{@case.id}")
     end
 
     it 'edits primary worker', :uuqa_1621 do
