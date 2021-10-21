32dee8ede12deb649802a304ea5cf0dd47ab6c5a
Sarah Rivas <sarah.r.rivas@gmail.com>
2021-10-21 16:32:31 -0400

    pays-1113 remove metafields no longer part of contracted service

--- spec/app_client/cases/add_constructed_service_spec.rb
+++ spec/app_client/cases/add_constructed_service_spec.rb
@@ -19,27 +19,9 @@ describe '[Payments]', :app_client, :payments do
     it 'adds a service', :pays_826 do
       starts_at = Faker::Date.between(from: Date.today, to: Date.today).strftime('%m/%d/%Y')
       unit_amount = Faker::Number.number(digits: 2)
-      toll_cost = Faker::Number.number(digits: 2)
-      origin_address = {
-        origin_address_line1: Faker::Address.street_address,
-        origin_address_line2: Faker::Address.secondary_address,
-        origin_city: Faker::Address.city,
-        origin_zip: '10015',
-        origin_state: Faker::Address.state
-      }
-      destination_address = {
-        destination_address_line1: Faker::Address.street_address,
-        destination_address_line2: Faker::Address.secondary_address,
-        destination_city: Faker::Address.city,
-        destination_zip: '10013',
-        destination_state: Faker::Address.state
-      }
       contracted_service_form_values = {
         unit_amount: unit_amount,
         starts_at: starts_at,
-        origin_address: origin_address,
-        destination_address: destination_address,
-        toll_cost: toll_cost
       }
 
       case_detail_page.click_contracted_service_button
