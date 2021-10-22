8fbe050a39f7626d892a4f7430add97f29a75365
Leah Garber <leah.garber88@gmail.com>
2021-10-22 14:54:54 -0400

    update description function for simplicity

--- spec/ehr/referrals/pages/add_description.rb
+++ spec/ehr/referrals/pages/add_description.rb
@@ -27,10 +27,9 @@ class AddDescription < BasePage
       is_displayed?(DESCRIPTION_INPUT)
   end
 
-  def fill_out_description_card_for_each_referral(description: nil)
+  def fill_out_description_card_for_each_referral(description: Faker::Lorem.word)
     referral_cards = find_elements(REFERRAL_CARD)
     referral_cards.each do |referral_card|
-      description ||= Faker::Lorem.word # set description if not passed as argument
       description_field = referral_card.find_element(DESCRIPTION_INPUT)
       description_field.send_keys description
       if (primary_worker_dropdown = referral_card.find_elements(PRIMARY_WORKER_SELECT_FIELD)[0])
