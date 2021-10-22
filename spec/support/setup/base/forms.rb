74131ee81627cad2fa7f8519160f10e63728ef31
Leah Garber <leah.garber88@gmail.com>
2021-10-22 09:46:53 -0400

    remove unneccessary variables in forms setup

--- spec/support/setup/base/forms.rb
+++ spec/support/setup/base/forms.rb
@@ -3,7 +3,7 @@ module Setup
     class << self
       def get_all_forms_for_case(token:, group_id:, contact_id:, case_id:)
         forms_response = Requests::Forms.get_all_for_case(token: token, group_id: group_id, contact_id: contact_id, case_id: case_id)
-        parsed_forms_response = JSON.parse(forms_response, object_class: OpenStruct).data
+        JSON.parse(forms_response, object_class: OpenStruct).data
       end
 
       def get_first_form_name_for_case(token:, group_id:, contact_id:, case_id:)
@@ -17,7 +17,7 @@ module Setup
 
       def get_all_forms_for_referral(token:, group_id:, referral_id:)
         forms_response = Requests::Forms.get_all_for_referral(token: token, group_id: group_id, referral_id: referral_id)
-        parsed_forms_response = JSON.parse(forms_response, object_class: OpenStruct).data
+        JSON.parse(forms_response, object_class: OpenStruct).data
       end
 
       def get_first_form_name_for_referral(token:, group_id:, referral_id:)
@@ -30,7 +30,7 @@ module Setup
 
       def get_all_screenings_for_provider(token:, group_id:, network_id:)
         screenings_response = Requests::Forms.get_screenings_for_provider(token: token, group_id: group_id, network_id: network_id)
-        parsed_screenings_response = JSON.parse(screenings_response, object_class: OpenStruct).data
+        JSON.parse(screenings_response, object_class: OpenStruct).data
       end
 
       def get_first_screening_id_for_provider(token:, group_id:, network_id:)
