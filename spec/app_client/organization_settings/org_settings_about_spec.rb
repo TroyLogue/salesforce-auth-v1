80d368a9d3e18b1b9e8f53be38a367a2074b64fa
tonydeng <dev.tonybd@gmail.com>
2021-12-09 12:33:54 -0500

    updated org settings spec tag

--- spec/app_client/organization_settings/org_settings_about_spec.rb
+++ spec/app_client/organization_settings/org_settings_about_spec.rb
@@ -15,7 +15,7 @@ describe '[Org Settings - About]', :new_org_settings, :app_client do
       org_settings_about.authenticate_and_navigate_to(token: @auth_token, path: '/organization/settings')
     end
 
-    it 'can see the org settings page' do
+    it 'can see the org settings page', :uuqa_810 do
       expect(org_settings_about.page_displayed?).to be_truthy
     end
 
@@ -25,35 +25,35 @@ describe '[Org Settings - About]', :new_org_settings, :app_client do
         expect(org_settings_edit_org.page_displayed?).to be_truthy
       end
 
-      it 'org description' do
+      it 'org description', :uuqa_810 do
         description = Faker::Lorem.word
         org_settings_edit_org.save_description(description)
         expect(org_settings_about.page_displayed?).to be_truthy
         expect(org_settings_about.get_description).to include(description)
       end
 
-      it 'org website' do
+      it 'org website', :uuqa_810 do
         web_url = Faker::Internet.url
         org_settings_edit_org.save_website(web_url)
         expect(org_settings_about.page_displayed?).to be_truthy
         expect(org_settings_about.get_website).to include(web_url)
       end
 
-      it 'org phone' do
+      it 'org phone', :uuqa_810 do
         phone = Faker::Number.number(digits: 10)
         org_settings_edit_org.save_phone(phone)
         expect(org_settings_about.page_displayed?).to be_truthy
         expect(org_settings_about.get_phones).to include(org_settings_about.number_to_phone_format(phone))
       end
 
-      it 'org email' do
+      it 'org email', :uuqa_810 do
         email = Faker::Internet.email
         org_settings_edit_org.save_email(email)
         expect(org_settings_about.page_displayed?).to be_truthy
         expect(org_settings_about.get_emails).to include(email)
       end
 
-      it 'org hours of operation' do
+      it 'org hours of operation', :uuqa_810 do
         time = "#{Faker::Number.between(from: 1, to: 10)}:00 AM"
         org_settings_edit_org.save_time(time)
         expect(org_settings_about.page_displayed?).to be_truthy
